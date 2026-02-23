0947f5ff6d2964f3f40c1871e42f3ed0c0327010
Castrokimaru <castrokimaru@gmail.com>
2026-02-23 08:55:03 +0300

    `Refactor and update frontend codebase with new design and features`

--- front-end/app/layout.tsx
+++ front-end/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GlobalBackButton } from "@/components/common/GlobalBackButton";
+import { RouteTransition } from "@/components/common/RouteTransition";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -30,7 +32,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <GlobalBackButton />
+        <RouteTransition>{children}</RouteTransition>
       </body>
     </html>
   );
