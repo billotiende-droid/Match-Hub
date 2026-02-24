b7512d67f86c693f870ae2e69e9fd68877fd99e9
billotiende-droid <bill.otiende@student.moringaschool.com>
2026-02-24 21:14:23 +0300

    implement naming convention for metadata and UUID generator

--- server/models.py
+++ server/models.py
@@ -12,3 +12,9 @@ convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
     "pk": "pk_%(table_name)s"
 }
+
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(metadata=metadata)
+
+def generate_uuid():
+    return str(uuid.uuid4())
