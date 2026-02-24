1c4ac92ad74b29582a6a188d419d6ceae38d279a
billotiende-droid <bill.otiende@student.moringaschool.com>
2026-02-24 21:11:34 +0300

    Essentials for PostgreSQL avoiding unnamed constraints

--- server/models.py
+++ server/models.py
@@ -3,3 +3,12 @@ from datetime import datetime, timezone
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData, Enum, UniqueConstraint, String
 from sqlalchemy.dialects.postgresql import UUID
+
+# Essential for PostgreSQL migrations to avoid "unnamed constraint" errors
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
